9a10afccb292c9b85d5d1b2c29cb5184f7ba3551
nemochriscjm <maskellc@hotmail.co.uk>
2026-08-05 18:47:03 +0100

    adminjsupdate

--- js/admin.js
+++ js/admin.js
@@ -2,25 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const menuButton = document.querySelector(".menu-toggle");
     const sidebar = document.querySelector(".sidebar");
-    const overlay = document.querySelector(".overlay");
 
-    if (!menuButton || !sidebar || !overlay) return;
+    if (!menuButton || !sidebar) return;
 
     menuButton.addEventListener("click", () => {
         sidebar.classList.toggle("open");
-        overlay.classList.toggle("show");
-    });
-
-    overlay.addEventListener("click", () => {
-        sidebar.classList.remove("open");
-        overlay.classList.remove("show");
-    });
-
-    window.addEventListener("resize", () => {
-        if (window.innerWidth > 900) {
-            sidebar.classList.remove("open");
-            overlay.classList.remove("show");
-        }
     });
 
 });
